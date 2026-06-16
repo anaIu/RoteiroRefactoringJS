7a346d8b2e36be7554dce4ed7c4d5bd32162d026
Ana Flavia Lunedo <analunedo5@gmail.com>
2026-06-16 09:58:39 -0300

    Commit 1 - Extração de Função

--- index.js
+++ index.js
@@ -7,11 +7,10 @@ function gerarFaturaStr (fatura, pecas) {
     const formato = new Intl.NumberFormat("pt-BR",
                           { style: "currency", currency: "BRL",
                             minimumFractionDigits: 2 }).format;
-  
-    for (let apre of fatura.apresentacoes) {
-      const peca = pecas[apre.id];
+
+    function calcularTotalApresentacao(apre, peca) {
       let total = 0;
-  
+
       switch (peca.tipo) {
       case "tragedia":
         total = 40000;
@@ -29,20 +28,29 @@ function gerarFaturaStr (fatura, pecas) {
       default:
           throw new Error(`Peça desconhecia: ${peca.tipo}`);
       }
-  
+
+      return total;
+    }
+
+    for (let apre of fatura.apresentacoes) {
+      const peca = pecas[apre.id];
+
+      let total = calcularTotalApresentacao(apre, peca);
+
       // créditos para próximas contratações
       creditos += Math.max(apre.audiencia - 30, 0);
-      if (peca.tipo === "comedia") 
+      if (peca.tipo === "comedia")
          creditos += Math.floor(apre.audiencia / 5);
-  
+
       // mais uma linha da fatura
       faturaStr += `  ${peca.nome}: ${formato(total/100)} (${apre.audiencia} assentos)\n`;
       totalFatura += total;
     }
+
     faturaStr += `Valor total: ${formato(totalFatura/100)}\n`;
     faturaStr += `Créditos acumulados: ${creditos} \n`;
     return faturaStr;
-  }
+}
 
 const faturas = JSON.parse(readFileSync('./faturas.json'));
 const pecas = JSON.parse(readFileSync('./pecas.json'));
